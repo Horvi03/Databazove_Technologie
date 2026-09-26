6347bf0ea60c48b2c725da93ac8fc4a17deac4ab
Martin Horváth <horvathmartin766@gmail.com>
2026-09-26 11:55:08 +0200

    Riesenie ulohy 10

---
+++ zadanie1a_uloha10.sql
@@ -0,0 +1,8 @@
+SELECT
+    c.customer_name,
+    SUM(o.sales) AS total_sales
+FROM customers c
+JOIN orders o
+    ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name
+HAVING SUM(o.sales) > 2000;
